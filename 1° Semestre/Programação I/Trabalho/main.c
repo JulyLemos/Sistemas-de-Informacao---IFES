9d849aed34e39477c35913c5d253a8ac18ca8edc
José Luiz <152568942+Adaicivi@users.noreply.github.com>
2026-06-24 22:54:20 -0300

    correção e add menu login

--- 1° Semestre/Programação I/Trabalho/main.c
+++ 1° Semestre/Programação I/Trabalho/main.c
@@ -6,21 +6,15 @@
 #include <string.h>
 #include <locale.h>
 
-//define a constante para essa versão se ainda não foi definida.
 #include <windows.h>
 #ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
     #define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
 #endif
 
-//importando as funções de login dos usuarios
-#include "usuarios/login.h"
+#include "projeto/usuarios/login.h"
+#include "projeto/funcoes/funcoes_guia.h"
 
-//importar funções dos menus
-#include "funcoes/funcoes_guia.h"
-
-
-
-int main(void){
+int main(void) {
 
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
     DWORD dwMode = 0;
@@ -28,120 +22,122 @@ int main(void){
     dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
     SetConsoleMode(hOut, dwMode);
 
-    setlocale(LC_ALL,"pt_BR.UTF-8");
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
 
-    char logindigitado[20], senhadigitada [20];
-    Login session;
+    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    Guia catalogo[MAX_GUIAS];
-
-    strcpy(catalogo[0].nome, "Pampas");
-    strcpy(catalogo[0].materia, "Geografia");
-    strcpy(catalogo[0].autor, "Diêgo Cavalcante");
-    catalogo[0].data = 5;
-    strcpy(catalogo[0].conteudo, "Pampas é lorem ipsum");
-    strcpy(catalogo[0].livro, "Brasil: um país de todos os biomas.");
-
-
-    strcpy(catalogo[1].nome, "Termodinâmica");
-    strcpy(catalogo[1].materia, "Física");
-    strcpy(catalogo[1].autor, "Maria Cardoso");
-    catalogo[1].data = 12;
-    strcpy(catalogo[1].conteudo, "Nós temos a dilatação...");
-    strcpy(catalogo[1].livro, "Física elementar");
-
-
-    int total = 2;
-
-    /* ---- Grava (cria o arquivo) e depois le de volta ---- */
-    gravarGuia(catalogo, total);
-
+    // Carrega os guias do arquivo (se não existir, começa vazio)
     Guia carregadas[MAX_GUIAS];
     int qtd = lerGuia(carregadas);
     printf(">> %d Guias carregada(s) do arquivo.\n", qtd);
 
-    //menu interativo
-    printf("\n\e[1;94m*-*-*-*-*-*-*- Entrar -*-*-*-*-*-*-*\033[0m\n");
-    printf(" Digite 0 para Cadastro e 1 para Login: ");
-    scanf("%d", );
-    
+    // Menu de acesso
+    int escolhaInicial = -1;
+    Login session;
+    char logindigitado[20], senhadigitada[20];
+    int autenticado = 0;
 
+    do {
+        printf("\n\e[1;94m*-*-*-*-*-*-*- Acesso -*-*-*-*-*-*-*\033[0m\n");
+        printf(" 1 - Cadastrar novo usuário\n");
+        printf(" 2 - Fazer login\n");
+        printf("\e[1;31m 0 - Sair\033[0m\n");
+        printf("Escolha: ");
+        scanf("%d", &escolhaInicial);
+        getchar();
 
-    // Tenta autenticar o usuário
-    if (autenticar(logindigitado, senhadigitada, &session)) {
-        printf("\n\nLogin feito com sucesso, seja bem vinde, %s!\n", session.usuario);
+        if (escolhaInicial == 1) {
+            cadastrarUsuario();
+        } else if (escolhaInicial == 2) {
+            printf("\nUsuário: ");
+            scanf("%s", logindigitado);
+            printf("Senha: ");
+            scanf("%s", senhadigitada);
+            getchar();
 
-        int opcao = -1; // Inicializa a variável de opção do menu
-        char termo[100];
-
-        // LOOP DO MENU INTERATIVO
-        do {
-            if (session.tipo == 1) { // --- MENU DO ADMIN ---
-                printf("\n============ MENU ADMIN ============\n");
-                printf("1 - Listar todas os guias\n");
-                printf("2 - Pesquisar (digitar um termo)\n");
-                printf("3 - Alterar um guia\n");
-                printf("4 - Excluir uma guia\n");
-                printf("5 - Criar novo guia\n");
-                printf("6 - Cadastro\n")
-                printf("0 - Sair\n");
-                printf("Escolha uma opcao: ");
-
-                scanf("%d", &opcao);
-                getchar();
-
-                if (opcao == 1) {
-                    for (int i = 0; i < qtd; i++) {
-                        imprimirGuia(carregadas[i]);
-                    }
-                } else if (opcao == 2) {
-                    printf("Digite o que deseja pesquisar: ");
-                    fgets(termo, 100, stdin);
-                    removerQuebraLinha(termo);
-                    pesquisarTexto(carregadas, qtd, termo);
-                } else if (opcao == 3) {
-                    alterarGuia(carregadas, qtd);
-                    gravarGuia(carregadas, qtd);
-                } else if (opcao == 4) {
-                    excluirGuia(carregadas, &qtd);
-                    gravarGuia(carregadas, qtd);
-                } else if (opcao == 5) {
-                    criarGuia(carregadas, &qtd);
-                    gravarGuia(carregadas, qtd);
-                } else if (opcao != 0) {
-                    printf("Opcao invalida.\n");
-                }
+            if (autenticar(logindigitado, senhadigitada, &session)) {
+                autenticado = 1;
+            } else {
+                printf("\nLogin incorreto. Tente novamente.\n");
             }
-            else if (session.tipo == 0) { // --- MENU DO ALUNO ---
-                printf("\n============ MENU ALUNO ============\n");
-                printf("1 - Listar todas os guias\n");
-                printf("2 - Pesquisar (digitar um termo)\n");
-                printf("0 - Sair\n");
-                printf("Escolha uma opcao: ");
+        } else if (escolhaInicial != 0) {
+            printf("Opcao inválida.\n");
+        }
 
-                scanf("%d", &opcao);
-                getchar();
+    } while (!autenticado && escolhaInicial != 0);
 
-                if (opcao == 1) {
-                    for (int i = 0; i < qtd; i++) {
-                        imprimirGuia(carregadas[i]);
-                    }
-                } else if (opcao == 2) {
-                    printf("Digite o que deseja pesquisar: ");
-                    fgets(termo, 100, stdin);
-                    removerQuebraLinha(termo);
-                    pesquisarTexto(carregadas, qtd, termo);
-                } else if (opcao != 0) {
-                    printf("Opcao invalida.\n");
-                }
-            }
-
-        } while (opcao != 0); // Continua no menu até digitar 0
-
-    } else {
-        // Esse else agora pertence perfeitamente ao 'if (autenticar...)' lá de cima
-        printf("\n\nLogin incorreto. Acesso negado.\n");
+    if (!autenticado) {
+        printf("\nEncerrando o programa.\n");
+        return 0;
     }
+
+    printf("\n\nLogin feito com sucesso, seja bem vindo, %s!\n", session.usuario);
+
+    // Menu principal
+    int opcao = -1;
+    char termo[100];
+
+    do {
+        if (session.tipo == 1) { // MENU ADMIN
+            printf("\n\e[1;94m============ MENU ADMIN ============\033[0m\n");
+            printf("1 - Listar todas os guias\n");
+            printf("2 - Pesquisar (digitar um termo)\n");
+            printf("3 - Alterar um guia\n");
+            printf("4 - Excluir uma guia\n");
+            printf("5 - Criar novo guia\n");
+            printf("6 - Cadastrar novo usuário\n");
+            printf("\e[1;31m0 - Sair\033[0m\n");
+            printf("Escolha uma opção: ");
+
+            scanf("%d", &opcao);
+            getchar();
+
+            if (opcao == 1) {
+                for (int i = 0; i < qtd; i++) imprimirGuia(carregadas[i]);
+            } else if (opcao == 2) {
+                printf("\nDigite o que deseja pesquisar: ");
+                fgets(termo, 100, stdin);
+                removerQuebraLinha(termo);
+                pesquisarTexto(carregadas, qtd, termo);
+            } else if (opcao == 3) {
+                alterarGuia(carregadas, qtd);
+                gravarGuia(carregadas, qtd);
+            } else if (opcao == 4) {
+                excluirGuia(carregadas, &qtd);
+                gravarGuia(carregadas, qtd);
+            } else if (opcao == 5) {
+                criarGuia(carregadas, &qtd);
+                gravarGuia(carregadas, qtd);
+            } else if (opcao == 6) {
+                cadastrarUsuario();
+            } else if (opcao != 0) {
+                printf("\nOpção inválida.\n");
+            }
+
+        } else { // MENU ALUNO
+            printf("\n\e[1;94m============ MENU ALUNO ============\033[0m\n");
+            printf("1 - Listar todas os guias\n");
+            printf("2 - Pesquisar (digitar um termo)\n");
+            printf("\e[1;31m0 - Sair\033[0m\n");
+            printf("Escolha uma opção: ");
+
+            scanf("%d", &opcao);
+            getchar();
+
+            if (opcao == 1) {
+                for (int i = 0; i < qtd; i++) imprimirGuia(carregadas[i]);
+            } else if (opcao == 2) {
+                printf("\nDigite o que deseja pesquisar: ");
+                fgets(termo, 100, stdin);
+                removerQuebraLinha(termo);
+                pesquisarTexto(carregadas, qtd, termo);
+            } else if (opcao != 0) {
+                printf("Opção inválida.\n");
+            }
+        }
+
+    } while (opcao != 0);
 
     printf("\nEncerrando o programa.\n");
     return 0;
